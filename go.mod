00b9fb19da6f166d44f39043eb3fd45056594ea0
Eduardo Ribeiro <eduardo20003131@gmail.com>
2024-11-03 02:10:46 -0300

    Tela inicial CTF, frontend de login e backend capturando as informações do usuario matricula e senha

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hanufu/ctf-einfo
+
+go 1.23.0
---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.ServeFile(w, r, "index.html")
+		return
+	}
+	fmt.Println("Post")
+	matricula := r.FormValue("matricula")
+	password := r.FormValue("password")
+
+	fmt.Printf("Matricula: %s Password: %s", matricula, password)
+	http.ServeFile(w, r, "index.html")
+}
+
+// func handleHiddenDir(w http.ResponseWriter, r *http.Request) {
+// 	// Caminho para o diretório "oculto"
+// 	hiddenDir := ".hidden"
+
+// 	// Verifica se o arquivo ou diretório existe
+// 	path := filepath.Join(hiddenDir, filepath.Clean(r.URL.Path[len("/.hidden/"):]))
+// 	_, err := os.Stat(path)
+// 	if os.IsNotExist(err) {
+// 		http.NotFound(w, r)
+// 		return
+// 	}
+
+// 	// Serve o arquivo do diretório "oculto"
+// 	http.ServeFile(w, r, path)
+// }
+
+func main() {
+	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
+
+	// Serve o HTML
+	http.HandleFunc("/", home)
+
+	fmt.Println("Server rodando em http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
